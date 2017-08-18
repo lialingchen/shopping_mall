cba4defad1c1b85f3c3fb62a0ac033aa598466a2
Lia <lialilingchen@gmail.com>
2017-08-18 12:08:09 +0000

    rspec finish

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,13 +1,40 @@
 class Cart
     attr_reader :items
-    def initialize
-       @items = [] 
+    def initialize(items = [])
+       @items = items 
     end
     
-   
+    def serialize
+        a= @items.map { |item|
+            {"product_id" => item.product_id, "quantity"=>item.quantity}
+            
+        }
+        # a= [] 
+        # @items.each do |item|
+        #     a << {"product_id" => item.product_id, "quantity" => item.quantity}
+        # end
+  
+        { "items" => a }
+    end
+    
+    def self.from_hash(hash)
+        if hash.nil? or hash["items"].nil?
+        #   Cart.new
+            new
+        else
+            new hash["items"].map{ |item|
+                CartItem.new(item["product_id"], item["quantity"])
+            }
+            # a = []
+            # hash["items"].each do |item|
+            # a << CartItem.new(item["product_id"], item["quantity"])
+            # end
+            # Cart.new(a)
+        end
+    end
     
     def add_item(product_id)
-        found_item = @items.find{ |item| 
+        found_item = items.find{ |item| 
         item.product_id == product_id 
         }
         if found_item
